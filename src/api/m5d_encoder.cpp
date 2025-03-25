ce3b62ce81882211846537cb0f516b8932f4bd53
nyasu3w <125797829+nyasu3w@users.noreply.github.com>
2025-03-25 22:59:44 +0900

    added methods to Encoder

--- src/api/m5d_encoder.cpp
+++ src/api/m5d_encoder.cpp
@@ -12,6 +12,22 @@ static void c_encoder_getcount(mrb_vm *vm, mrb_value *v, int argc) {
     SET_INT_RETURN(encoder.getCount());
 }  
 
+static void c_encoder_clearcount(mrb_vm *vm, mrb_value *v, int argc) {
+    SET_INT_RETURN(encoder.clearCount());
+}
+
+static void c_encoder_setcount(mrb_vm *vm, mrb_value *v, int argc) {
+    int count = GET_INT_ARG(1);
+    encoder.setCount(count);
+}
+
+static void c_encoder_was_moved(mrb_vm *vm, mrb_value *v, int argc) {
+    if(encoder.wasMoved()) {
+        SET_TRUE_RETURN();
+    } else {
+        SET_FALSE_RETURN();
+    }
+}
 
 void api_encoder_define(void){
     encoder.attachHalfQuad(DIAL_ENCODER_PIN_A, DIAL_ENCODER_PIN_B);
@@ -19,4 +35,6 @@ void api_encoder_define(void){
 
     mrbc_define_class(0, "Encoder", mrbc_class_object);
     mrbc_define_method(0, mrbc_class_object, "get_count", c_encoder_getcount);
+    mrbc_define_method(0, mrbc_class_object, "clear_count", c_encoder_clearcount);
+    mrbc_define_method(0, mrbc_class_object, "set_count", c_encoder_setcount);
 }
